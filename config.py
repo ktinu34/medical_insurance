8bdc35d9bd4b2871e82f749a4387ab72e34eaec7
ktinu34 <kumar.tinu05@gmail.com>
2023-07-11 12:46:23 +0530

    updated

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ PORT_NUMBER = 8080
 MONGODB_PORT_NUMBER = 27017
 
 
-MODEL_FILE_PATH = 'static\models\linear_regression.pkl'
+MODEL_FILE_PATH = r'static\models\linear_regression.pkl'
 
-JSON_FILE_PATH = 'static\json_files\proj_data.json'
+JSON_FILE_PATH = r'static\json_files\proj_data.json'
