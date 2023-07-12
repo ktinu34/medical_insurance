029dce7a59fe62a665d1cc78c2363eb8e40099ff
ktinu34 <138862541+ktinu34@users.noreply.github.com>
2023-07-12 13:34:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,3 +9,4 @@ MONGODB_PORT_NUMBER = 27017
 MODEL_FILE_PATH = os.path.join("static","models","linear_regression.pkl")
 
 JSON_FILE_PATH = os.path.join("static","json_files","proj_data.json")
+print('rishabh')
